0d306b7b0e0329a6f4832600e947f94c727b66b6
eduardoguevarasw <edugvra@outlook.com>
2023-02-23 18:21:14 -0500

    idtour

---
+++ public/js/selectour.js
@@ -0,0 +1,15 @@
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdmdmxqendwemljd3lucW1pcnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc2NTU5NjcsImV4cCI6MTk4MzIzMTk2N30.Jj6AQlRlabhEBppjaP9Bw0kBa77HHOBTTLNsy5cv2EY";
+const url = "https://gfvljzwpzicwynqmirui.supabase.co";
+const database = supabase.createClient(url, key);
+
+//ocultar el boton de comprar boleto de paypal 
+document.getElementById("paypal-button-container").style.display = "none";
+
+//cerrar sesion si hizo click 
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.href = "https://eduardoguevarasw.github.io/sachawassi/";
+})
+
+//mostrar información del tour
